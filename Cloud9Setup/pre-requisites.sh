abe2b23bc8463644e27eb79bbbd9039773f12f04
Zee Khoo <zeekhoo@gmail.com>
2023-06-13 17:45:19 -0700

    updated Cloud9 setup script

--- Cloud9Setup/pre-requisites.sh
+++ Cloud9Setup/pre-requisites.sh
@@ -1,26 +1,34 @@
 #!/bin/bash -x
 . /home/ec2-user/.nvm/nvm.sh
 
-#Install python3.8
-sudo yum install -y amazon-linux-extras
-sudo amazon-linux-extras enable python3.8
-sudo yum install -y python3.8
-sudo alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
-sudo alternatives --set python3 /usr/bin/python3.8
+# Install python3.9. See: https://tecadmin.net/install-python-3-9-on-amazon-linux/
+sudo yum -y install gcc openssl-devel bzip2-devel libffi-devel 
+wget https://www.python.org/ftp/python/3.9.16/Python-3.9.16.tgz 
+sudo tar xzf Python-3.9.16.tgz
+cd Python-3.9.16 
+sudo ./configure --enable-optimizations 
+sudo make altinstall 
+cd ..
+rm Python-3.9.16.tgz
+sudo alternatives --install /usr/bin/python3 python3 /usr/local/bin/python3.9 1
+sudo alternatives --set python3 /usr/local/bin/python3.9
 
-# Uninstall aws cli v1 and Install aws cli version-2.3.0
+# Uninstall default aws cli and Install aws cli version-2.9.5
 sudo pip2 uninstall awscli -y
 
-echo "Installing aws cli version-2.3.0"
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.3.0.zip" -o "awscliv2.zip"
+echo "Installing aws cli version-2.9.5"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.9.5.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
 rm awscliv2.zip
-rm -rf aws 
+rm -rf aws
+sudo alternatives --install /usr/bin/aws aws /usr/local/bin/aws 1
+sudo alternatives --set aws /usr/local/bin/aws
 
-# Install sam cli version 1.33.0
-echo "Installing sam cli version 1.33.0"
-wget https://github.com/aws/aws-sam-cli/releases/download/v1.33.0/aws-sam-cli-linux-x86_64.zip
+# Install sam cli 
+echo "Installing sam cli version 1.82.0"
+wget https://github.com/aws/aws-sam-cli/releases/download/v1.82.0/aws-sam-cli-linux-x86_64.zip
+# wget https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip
 unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
 sudo ./sam-installation/install
 if [ $? -ne 0 ]; then
@@ -33,35 +41,25 @@ fi
 rm aws-sam-cli-linux-x86_64.zip
 rm -rf sam-installation
 
-# Install git-remote-codecommit version 1.15.1
-echo "Installing git-remote-codecommit version 1.15.1"
-curl -O https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py --user
-rm get-pip.py
-
-python3 -m pip install git-remote-codecommit==1.15.1
+# Install git-remote-codecommit version 1.16
+echo "Installing git-remote-codecommit version 1.16"
+python3 -m pip install git-remote-codecommit==1.16
 
 # Install node v14.18.1
 echo "Installing node v14.18.1"
 nvm deactivate
 nvm uninstall node
-nvm install v14.18.1
-nvm use v14.18.1
-nvm alias default v14.18.1
+nvm install v16.20.0
+nvm use v16.20.0
+nvm alias default v16.20.0
 
+# Install cdk cli version 2.32.1
+# echo "Installing cdk cli version 2.32.1"
+# npm uninstall -g aws-cdk
+# npm install -g aws-cdk@2.32.1
 
-# Install cdk cli version 1.129.0
-echo "Installing cdk cli version 1.129.0"
-npm uninstall -g aws-cdk
-npm install -g aws-cdk@1.129.0
-
-# Install angular version 12.1.1
-echo "Installing angular version 12.1.1"
-export NG_CLI_ANALYTICS=ci
-npm install -g @angular/cli@12.1.1
-
-#Install jq version 1.5
+# Install jq version 1.5
 sudo yum -y install jq-1.5
 
-#Install pylint version 2.11.1
-python3 -m pip install pylint==2.11.1
+#Install pylint version 2.15.8
+python3 -m pip install pylint==2.15.8
